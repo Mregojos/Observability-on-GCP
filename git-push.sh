5ead57cdc6ec49467ebb3a256e2b9df788416c13
Matt <mattiregojos@gmail.com>
2023-12-10 03:31:26 +0000

    Add and modify files

--- git-push.sh
+++ git-push.sh
@@ -9,16 +9,21 @@ rm -rf ./*/*/.env.*
 rm -rf .ipynb_checkpoints
 rm -rf ./*/.ipynb_checkpoints
 rm -rf ./*/*/.ipynb_checkpoints
-echo """
-rm -rf ./app.yaml
-rm -rf ./*/app.yaml
-Yes or No:
-"""
-read USER_INPUT
-if $USER_INPUT = "Yes"; then
-    rm -rf ./app.yaml
-    rm -rf ./*/app.yaml
-fi
+
+#echo """
+#rm -rf ./app.yaml
+#rm -rf ./*/app.yaml
+#Yes or No:
+#"""
+# USER_INPUT
+#export USER_INPUT=$USER_INPUT
+#if $USER_INPUT = "Yes"; then
+#    rm -rf ./app.yaml
+#    rm -rf ./*/app.yaml
+#fi
+
+# rm -rf ./app.yaml
+# rm -rf ./*/app.yaml
 echo "Successfully removed .env.* and checkpoints files ready to be pushed to repository."
 
 git add .
